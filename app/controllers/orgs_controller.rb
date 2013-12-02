75cfa3eb2af1700661dce28f40282e9b0a05d6f4
Chad Jemmett <chad.jemmett@gmail.com>
2013-12-02 08:58:47 -0700

    can save org name and demo status to db

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -1,6 +1,21 @@
 class OrgsController < ApplicationController
 
   def new
-
+    @org = Org.new
+    @org.demo = true
   end
+
+  def create
+    @org = Org.new(params[:org])
+    @org.demo = true
+    @org.save
+    redirect_to @org
+  end
+
+  def show
+    @org = Org.find(params[:id])
+  end
+
+
+
 end
---
+++ db/migrate/20131202143324_create_orgs.rb
@@ -0,0 +1,10 @@
+class CreateOrgs < ActiveRecord::Migration
+  def change
+    create_table :orgs do |t|
+      t.text :name
+      t.boolean :demo
+
+      t.timestamps
+    end
+  end
+end
