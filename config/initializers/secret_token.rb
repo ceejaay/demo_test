953281060db27ea427e175aa9772a6f96c4b96a4
Chad Jemmett <chad.jemmett@gmail.com>
2013-12-02 07:20:04 -0700

    first commit, change readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoTest::Application.config.secret_token = '13b37fb417276c3fbbeec2f937273c210cd147d6a3ed5b9105eaa0cb744e7d6ff58b916acc4acfc59357c3e80c813fb4dca73aee144b8b2adfb3305490f9b283'
